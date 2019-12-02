b29cafe5a83019b9564b2637d2006298c7544ef5
kolujAs <fernandodeibe@gmail.com>
2019-12-02 18:23:53 -0300

    sitio en construccion

--- resources/views/web/construccion.blade.php
+++ resources/views/web/construccion.blade.php
@@ -13,15 +13,63 @@
 
         <!-- Bootstrap 4.3.1-->
         <!-- <link href="{{asset('css/bootstrap/bootstrap.min.css')}}" rel="stylesheet"> -->
-
+      
+	    <link href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" rel="stylesheet">
+	
         <!-- Mi CSS -->
         <link href="{{ asset('css/index.css') }}" rel="stylesheet">       
-        <link href="{{ asset('css/web/construccion.css') }}" rel="stylesheet">       
-
+        <link href="{{ asset('css/web/construccion.css') }}" rel="stylesheet">     
         <title>Title</title>
+        <style>
+            .logo{
+                width: 10rem;
+                border-radius: .75rem;
+                margin: 2rem 0;
+            }
+
+            h1{
+                font-size: 2.5rem !important;
+            }
+
+            h2{
+                font-size: 1.3rem !important;
+            }
+
+            h3{
+                font-size: 1.3rem !important;
+            }
+
+                /* MD */
+                @media(min-width: 768px){
+                .logo{
+                    width: 12rem;
+                    border-radius: .75rem;
+                }
+            }
+
+            @media(min-width: 1024px){
+            .logo{
+                width: 12rem;
+                border-radius: .75rem;
+            }
+
+                
+            h1{
+                font-size: 3rem !important;
+            }
+
+            h2{
+                font-size: 1.5rem !important;
+            }
+
+            h3{
+                font-size: 1.5rem !important;
+            }
+            }
+        </style>
     </head>
     <body>
-        <div class="container-fluid">
+       <!--  <div class="container-fluid">
             <div class="row">
                 <div class="background-construction">
                 <h1>Title</h1>
@@ -33,6 +81,26 @@
                             <p class="card-text">ejemplo@correo.com</p>
                         </div>
                     </div>
+                </div>
+            </div>
+        </div> -->
+
+        <div class="coverPage">
+        
+            <div class="__coverPage-content">
+            <img src="/img/logo.jpg" class="img-fluid logo" alt="Responsive image">
+                <h1>Axial Grupo Medico</h1>
+                <h2>Medicina especializada en Columna</h2>
+                <h3>Sitio web en construcción</h3>
+                <p>info@grupoaxial.org <br>
+                (011) 4772-7679 <br>
+                Huergo 366 6 "A" , Cañitas, CABA</p>
+                <div class="socialMedia">
+                    <a class="socialIcon" href="mailto:yourEmail@yourDomain.com"><i class="fas fa-envelope"></i></a>
+                    <a target="_blank" class="socialIcon" href="https://wa.me/+541123539011">
+                        <i class="fab fa-whatsapp"></i>
+                    </a>
+                    <a class="socialIcon" href="#"><i class="fab fa-linkedin-in"></i></a>
                 </div>
             </div>
         </div>
