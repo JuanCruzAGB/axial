e11a10bc250d6b49f32f4bbf9a69dc0cb93c0e96
kolujAs <fernandodeibe@gmail.com>
2020-01-13 17:04:52 -0300

    footer axial

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,31 @@
+<!-- Footer -->
+<section id="footer">
+			<div class="row text-center text-center pt-4 d-flex justify-content-center">
+				<div class="col-xs-12 col-sm-4 col-md-4">
+					<h5>Quick links</h5>
+					<ul class="list-unstyled quick-links">
+						<li><a href=""><i class="fa fa-angle-double-right"></i>Inicio</a></li>
+						<li><a href=""><i class="fa fa-angle-double-right"></i>Nosotros</a></li>
+						<li><a href=""><i class="fa fa-angle-double-right"></i>Cirugia mini invasiva</a></li>
+						<li><a href=""><i class="fa fa-angle-double-right"></i>Patologia de la columna</a></li>
+						<li><a href=""><i class="fa fa-angle-double-right"></i>Equipo</a></li>
+						<li><a href=""><i class="fa fa-angle-double-right"></i>Noticias</a></li>
+						<li><a href=""><i class="fa fa-angle-double-right"></i>Panel</a></li>
+						<li><a href=""><i class="fa fa-angle-double-right"></i>Cerrar sesión</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-12 mt-2 mt-sm-5">
+					<ul class="list-unstyled list-inline social text-center">
+						<li class="list-inline-item"><a href=""><i class="fa fa-facebook"></i></a></li>
+						<li class="list-inline-item"><a href=""><i class="fa fa-twitter"></i></a></li>
+						<li class="list-inline-item"><a href=""><i class="fa fa-instagram"></i></a></li>
+						<li class="list-inline-item"><a href=""><i class="fa fa-google-plus"></i></a></li>
+						<li class="list-inline-item"><a href="" target="_blank"><i class="fa fa-envelope"></i></a></li>
+					</ul>
+				</div>
+				</hr>
+			</div>	
+	</section>
+	<!-- ./Footer -->
