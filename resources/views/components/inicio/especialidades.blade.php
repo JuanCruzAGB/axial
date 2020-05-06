69d6dcc23a4a471a9cb154decdb4618d31d0f155
JuanCruzAGB <juancarmentia@gmail.com>
2020-05-06 15:34:49 -0300

    Iconos de especialidades cambiados

--- resources/views/components/inicio/especialidades.blade.php
+++ resources/views/components/inicio/especialidades.blade.php
@@ -9,7 +9,7 @@
         <div class="cards row d-flex justify-content-center px-0 mx-auto">
             <div class="cartas-servicios invisible card col-12 col-md-5 col-lg-4 text-center border-0 mb-4">
                 <div class="card-body">
-                    <i class="icon-cuerpo mb-3 iconos-servicios icon"></i>
+                    <i class="icon-columna-1 mb-3 iconos-servicios icon"></i>
                     <h3 class="card-title mb-3">Escoliosis – Cifosis</h3>
                     <a class="btn btn-secondary" href="/especialidades#escoliosis-cifosis">
                         <i class="fas fa-chevron-right pr-2 pb-0"></i> Ver más
@@ -36,7 +36,7 @@
             </div>
             <div class="cartas-servicios invisible card col-12 col-md-5 col-lg-4 text-center border-0 mb-4">
                 <div class="card-body">
-                    <i class="icon-masaje mb-3 iconos-servicios icon"></i>
+                    <i class="icon-rayos mb-3 iconos-servicios icon"></i>
                     <h3 class="card-title mb-3">Evaluación interdisciplinaria</h3>
                     <a class="btn btn-secondary" href="/especialidades#evaluacion-interdisciplinaria">
                         <i class="fas fa-chevron-right pr-2 pb-0"></i> Ver más
@@ -45,7 +45,7 @@
             </div>
             <div class="cartas-servicios invisible card col-12 col-md-5 col-lg-4 text-center border-0 mb-4">
                 <div class="card-body">
-                    <i class="icon-cuello mb-3 iconos-servicios icon"></i>
+                    <i class="icon-atras mb-3 iconos-servicios icon"></i>
                     <h3 class="card-title mb-3">Descripción generada automáticamente Columna cervical</h3>
                     <a class="btn btn-secondary" href="/especialidades#descripcion-generada-automaticamente-columna-">
                         <i class="fas fa-chevron-right pr-2 pb-0"></i> Ver más
@@ -63,7 +63,7 @@
             </div>
             <div class="cartas-servicios invisible card col-12 col-md-5 col-lg-4 text-center border-0 mb-4">
                 <div class="card-body">
-                    <i class="icon-cerebro mb-3 iconos-servicios icon"></i>
+                    <i class="icon-masaje mb-3 iconos-servicios icon"></i>
                     <h3 class="card-title mb-3">Kinesiología y Actividad física</h3>
                     <a class="btn btn-secondary" href="/especialidades#kinesiologia-y-actividad-fisica">
                         <i class="fas fa-chevron-right pr-2 pb-0"></i> Ver más
@@ -72,7 +72,7 @@
             </div>
             <div class="cartas-servicios invisible card col-12 col-md-5 col-lg-4 text-center border-0 mb-4">
                 <div class="card-body">
-                    <i class="icon-atras mb-3 iconos-servicios icon"></i>
+                    <i class="icon-cerebro mb-3 iconos-servicios icon"></i>
                     <h3 class="card-title mb-3">Intervencionismo asistido por imágenes</h3>
                     <a class="btn btn-secondary" href="/especialidades#intervencionismo-asistido-por-imagenes">
                         <i class="fas fa-chevron-right pr-2 pb-0"></i> Ver más
