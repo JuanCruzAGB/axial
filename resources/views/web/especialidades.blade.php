dff1d98b4d2088ea536d60d7dbde0ee8eaafc0c3
JuanCruzAGB <juancarmentia@gmail.com>
2020-05-12 14:09:31 -0300

    Imagen cambiada en "Especialidades"

--- resources/views/web/especialidades.blade.php
+++ resources/views/web/especialidades.blade.php
@@ -82,35 +82,35 @@
                     <h2 class="text-center text-lg-left mb-3 mb-3">Escoliosis – Cifosis</h2>
                     <p class="m-0 mb-3 p-0">Somos un equipo altamente especializados en deformidades de la columna vertebral. Participamos en forma permanente en cursos de formación médica como participantes y expositores. Escoliosis y cifosis son algunas de las patologías mas relevantes al referirnos a curvas patológicas de la columna vertebral. Evaluamos, diagnosticamos y tratamos a pacientes pediátricos y adultos de forma interdisciplinaria.</p>
                     <div class="imagen-servicio d-flex justify-content-center px-0">
-                        <img src="/img/servicios/random.jpg" alt="Imagen del servicio">
+                        <img src="/img/especialidades/01-doctor.jpg" alt="Imagen del servicio">
                     </div>
                 </div>
                 <div id="cirugias-minimamente-invasivas" class="content px-3 px-lg-0 pl-lg-5">
                     <h2 class="text-center text-lg-left mb-3">Cirugías Mínimamente Invasiva</h2>
                     <p class="m-0 mb-3 p-0">Las modernas técnicas quirúrgicas, cada vez menos agresivas, utilizan incisiones de pocos milímetros para introducir minúsculos instrumentos, cuyo manejo es controlado en una pantalla de alta definición o por medio de visión aumentada a través de un microscopio. Las mensionadas técnicas quirúrgicas son de utilidad para tratar una gran cantidad de patologías de la columna vertebral tanto en el pacientes adultos como en pacientes pediátricos.</p>
                     <div class="imagen-servicio d-flex justify-content-center px-0">
-                        <img src="/img/servicios/random.jpg" alt="Imagen del servicio">
+                        <img src="/img/especialidades/01-doctor.jpg" alt="Imagen del servicio">
                     </div>
                 </div>
                 <div id="departamento-de-cirugia-pediatrica-de-la-columna-vertebral" class="content px-3 px-lg-0 pl-lg-5">
                     <h2 class="text-center text-lg-left mb-3">Departamento de cirugía pediátrica de la columna vertebral</h2>
                     <p class="m-0 mb-3 p-0">Una de nuestras fortalezas es la formación médica de todos los integrantes del equipo en enfermedades pediátricas. Participamos como referentes de la patología en cursos nacionales e internacionales. Evaluamos, diagnosticamos y tratamos todas las patologías pediátricas que afectan la columna vertebral. Trabajamos permanentemente de forma interdisciplinaria disponiendo de todas las especialidades relacionadas a las patologías que afectan la columna vertebral.</p>
                     <div class="imagen-servicio d-flex justify-content-center px-0">
-                        <img src="/img/servicios/random.jpg" alt="Imagen del servicio">
+                        <img src="/img/especialidades/01-doctor.jpg" alt="Imagen del servicio">
                     </div>
                 </div>
                 <div id="evaluacion-interdisciplinaria" class="content px-3 px-lg-0 pl-lg-5">
                     <h2 class="text-center text-lg-left mb-3">Evaluación interdisciplinaria</h2>
                     <p class="m-0 mb-3 p-0">Pediatras, neumonólogos, endoscopistas, nutricionistas, anestesiólogos, psicólogos, clínicos, kinesiólogos, cirujano y/o cualquier otra especialidad que sea requerida según la enfermedad del paciente; nos reunimos para evaluar diagnosticar y definir conductas a seguir ante patologías complejas. Creemos que el trabajo en equipo potencia las habilidades individuales redundando en beneficios para los pacientes y sus familias.</p>
                     <div class="imagen-servicio d-flex justify-content-center px-0">
-                        <img src="/img/servicios/random.jpg" alt="Imagen del servicio">
+                        <img src="/img/especialidades/01-doctor.jpg" alt="Imagen del servicio">
                     </div>
                 </div>
                 <div id="descripcion-generada-automaticamente-columna-cervical" class="content px-3 px-lg-0 pl-lg-5">
                     <h2 class="text-center text-lg-left mb-3">Descripción generada automáticamente Columna cervical</h2>
                     <p class="m-0 mb-3 p-0">La columna cervical es asiento de múltiples patologías, por envejecimiento, congénitas, traumáticas, tumorales, infecciosas y otras. El trabajo en equipo entre traumatólogos y neurocirujanos nos brindan un abordaje integral de las patología que alteran la columna cervical tanto en adultos como en niños..</p>
                     <div class="imagen-servicio d-flex justify-content-center px-0">
-                        <img src="/img/servicios/random.jpg" alt="Imagen del servicio">
+                        <img src="/img/especialidades/01-doctor.jpg" alt="Imagen del servicio">
                     </div>
                 </div>
                 <div id="columna-lumbar" class="content px-3 px-lg-0 pl-lg-5">
@@ -118,14 +118,14 @@
                     <p class="m-0 mb-3 p-0">La región lumbar es donde asienten la mayor cantidad de patologías del adulto. Siendo el dolor lumbar la consulta mas frecuente. Canal estrecho, hernias de discos, espondilolistesis, escoliosis y fracturas son algunas de las patologías que afectan la columna lumbar.
                     La visión conjunta entre traumatólogos, neurocirujanos, kinesiólogos y entrenadores físicos que siempre realizamos en nuestro equipo, nos permite considerar los mejores tratamientos para cada dolencia que afecta la región lumbar.</p>
                     <div class="imagen-servicio d-flex justify-content-center px-0">
-                        <img src="/img/servicios/random.jpg" alt="Imagen del servicio">
+                        <img src="/img/especialidades/01-doctor.jpg" alt="Imagen del servicio">
                     </div>
                 </div>
                 <div id="kinesiologia-y-actividad-fisica" class="content px-3 px-lg-0 pl-lg-5">
                     <h2 class="text-center text-lg-left mb-3">Kinesiología y Actividad física</h2>
                     <p class="m-0 mb-3 p-0">El equipo de Axial considera indispensable interactuar con kinesiólogos y entrenadores físicos para el tratamiento de las patologías que afectan a la columna vertebral. La interdisciplina con diálogo permanente sobre la evolución de los pacientes permite adecuar las terapéuticas y consecuentemente obtener una mejor y más temprana recuperación. Nuestro equipo está especializado tanto en pacientes adultos como pediátricos.</p>
                     <div class="imagen-servicio d-flex justify-content-center px-0">
-                        <img src="/img/servicios/random.jpg" alt="Imagen del servicio">
+                        <img src="/img/especialidades/01-doctor.jpg" alt="Imagen del servicio">
                     </div>
                 </div>
                 <div id="intervencionismo-asistido-por-imagenes" class="content px-3 px-lg-0 pl-lg-5">
@@ -133,7 +133,7 @@
                     <p class="m-0 mb-3 p-0">Contamos con un equipo altamente especializado en las más modernas técnicas de intervencionismos asistidas por imágenes de alta resolución.
                     Son una herramienta indispensable, en continuo crecimiento y desarrollo. Son de utilidad para diagnósticos y tratamientos de un gran número de patologías en toda la extensión de la columna vertebral. Son aplicadas tanto en patologías del adulto como en niños.</p>
                     <div class="imagen-servicio d-flex justify-content-center px-0">
-                        <img src="/img/servicios/random.jpg" alt="Imagen del servicio">
+                        <img src="/img/especialidades/01-doctor.jpg" alt="Imagen del servicio">
                     </div>
                 </div>
             </div>
